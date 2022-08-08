7e28a6285309aeec0a090bedfa8c03091c4fceae
zhuyijing <zhuyijing168@163.com>
2022-08-08 14:50:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"msg": "Welcome to Home Page"}
+    return {"msg": "Welcome to Home Page, from {}".format(os.environ.get('SECRET_USER', 'anonymous'))}
 
 
 @app.get("/items/{item_id}")
